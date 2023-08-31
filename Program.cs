b4237b92d76ee83365fbb33f8150fff9d6ca890a
Jasper <japsu.honkasalo@gmail.com>
2023-08-31 08:52:00 +0300

    Initial working commit

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace DynmapMarkerPrune
             REMOVE_SELECTION_EXCLUSIVE
         }
         
-        private const MarkerPruneMode MODE = MarkerPruneMode.REMOVE_SELECTION_INCLUSIVE;
+        private const MarkerPruneMode PRUNE_MODE = MarkerPruneMode.REMOVE_SELECTION_EXCLUSIVE;
         
         private const string MARKERS_YAML_FILE_PATH = "markers.yml";
         private const string MARKERS_YAML_OUTPUT_FILE_PATH = "markers_out.yml";
@@ -24,62 +24,58 @@ namespace DynmapMarkerPrune
         private static IOrderedDictionary<YamlNode, YamlNode> circles;
         private static IOrderedDictionary<YamlNode, YamlNode> markers;
 
-        private static int processedChunks;
-        private static int processedEntryCount;
+        private static int processedMarkersCount;
 
         private static readonly YamlScalarNode WorldNodeReference = new YamlScalarNode("world");
-        private static readonly HashSet<string> MarkersToDelete = new HashSet<string>();
+        private static readonly Dictionary<string, string> MarkersToDelete = new Dictionary<string, string>();
 
 
         private static void Main()
         {
-            LoadMarkers();
-
-            Console.WriteLine("Markers loaded...");
+            if (!File.Exists(MARKERS_YAML_FILE_PATH))
+            {
+                Console.WriteLine("Missing markers.yml -file.");
+                return;
+            }
+            
+            LoadAllMarkers();
 
             // Load regions.csv
             string[] selectedRegionsAndChunks = File.ReadAllLines(REGIONS_FILE_PATH);
 
-            int toProcessCount = selectedRegionsAndChunks.Length;
+            int markersTotalCount = circles.Count + markers.Count;
 
-            // Parse and process selected regions and chunks
-            foreach (string line in selectedRegionsAndChunks)
+            foreach (KeyValuePair<YamlNode,YamlNode> markerNode in circles)
             {
-                string[] parts = line.Split(';');
-                if (parts.Length == 2)
-                {
-                    // This is a region
-                    int regionX = int.Parse(parts[0]);
-                    int regionZ = int.Parse(parts[1]);
-                    ProcessMarkersInRegion(regionX, regionZ);
-                }
-                else if (parts.Length == 4)
-                {
-                    // This is a chunk
-                    // int regionX = int.Parse(parts[0]);
-                    // int regionZ = int.Parse(parts[1]);
-                    int chunkX = int.Parse(parts[2]);
-                    int chunkZ = int.Parse(parts[3]);
-                    ProcessMarkersInChunk(chunkX, chunkZ);
-                }
+                ProcessMarkerNode(markerNode, selectedRegionsAndChunks);
 
-                processedEntryCount++;
+                processedMarkersCount++;
                 
-                if(processedChunks % 1000 == 0)
-                    Console.WriteLine($"Processed {processedChunks} chunks ( {processedEntryCount / (double)toProcessCount * 100d:F1}% )...");
+                if(processedMarkersCount % 5 == 0)
+                    Console.WriteLine($"Processed {processedMarkersCount} markers ( {processedMarkersCount / (double)markersTotalCount * 100d:F1}% )...");
+            }
+
+            foreach (KeyValuePair<YamlNode,YamlNode> markerNode in markers)
+            {
+                ProcessMarkerNode(markerNode, selectedRegionsAndChunks);
+
+                processedMarkersCount++;
+                
+                if(processedMarkersCount % 5 == 0)
+                    Console.WriteLine($"Processed {processedMarkersCount} markers ( {processedMarkersCount / (double)markersTotalCount * 100d:F1}% )...");
             }
             
             // Open a StreamWriter to the file
-            using (StreamWriter writer = File.CreateText("output.txt"))
+            using (StreamWriter writer = File.CreateText("removed_marker_ids.txt"))
             {
                 // Iterate through the HashSet and write each element to the file
-                foreach (string item in MarkersToDelete)
+                foreach (KeyValuePair<string, string> pair in MarkersToDelete)
                 {
-                    writer.WriteLine(item);
+                    writer.WriteLine($"{pair.Value} \t (id {pair.Key})");
                 }
             }
 
-            Console.WriteLine($"Found {MarkersToDelete.Count} markers to delete from {processedChunks} deleted chunks.");
+            Console.WriteLine($"Found {MarkersToDelete.Count} markers to delete out of {markersTotalCount} markers in total.");
                 
             SaveModifiedYamlWithDeletedMarkers(MARKERS_YAML_OUTPUT_FILE_PATH);
             
@@ -88,7 +84,70 @@ namespace DynmapMarkerPrune
         }
 
 
-        private static void LoadMarkers()
+        private static void ProcessMarkerNode(KeyValuePair<YamlNode, YamlNode> markerNode, string[] selectedRegionsAndChunks)
+        {
+            YamlMappingNode properties = (YamlMappingNode)markerNode.Value;
+
+            if (!properties["world"].Equals(WorldNodeReference))
+                return;
+
+            (int x, int z) = GetCoordinates(properties);
+
+            foreach (string line in selectedRegionsAndChunks)
+            {
+                bool isInclusive;
+
+                string[] parts = line.Split(';');
+                switch (parts.Length)
+                {
+                    case 2:
+                    {
+                        // This is a region
+                        int regionX = int.Parse(parts[0]);
+                        int regionZ = int.Parse(parts[1]);
+
+                        isInclusive = IsMarkerInRegion(regionX, regionZ, x, z);
+                        break;
+                    }
+                    case 4:
+                    {
+                        // This is a chunk
+                        int chunkX = int.Parse(parts[2]);
+                        int chunkZ = int.Parse(parts[3]);
+
+                        isInclusive = IsMarkerInChunk(chunkX, chunkZ, x, z);
+                        break;
+                    }
+                    default:
+                        throw new Exception("What?");
+                }
+
+                if (isInclusive)
+                {
+                    if(PRUNE_MODE == MarkerPruneMode.REMOVE_SELECTION_INCLUSIVE)
+                        AddMarkerForDeletion(markerNode, properties, x, z);
+                    
+                    return;
+                }
+            }
+            
+            if(PRUNE_MODE == MarkerPruneMode.REMOVE_SELECTION_EXCLUSIVE)
+                AddMarkerForDeletion(markerNode, properties, x, z);
+        }
+
+
+        private static void AddMarkerForDeletion(KeyValuePair<YamlNode, YamlNode> markerNode, YamlMappingNode properties, int x, int z)
+        {
+            YamlNode key = markerNode.Key;
+            string label = properties["label"].ToString();
+
+            MarkersToDelete.Add(key.ToString(), label);
+
+            Console.WriteLine($"Found marker to delete: {key}   (Label: {label} | X: {x}, Z: {z})");
+        }
+
+
+        private static void LoadAllMarkers()
         {
             // Read the YAML file
             string markersYamlContent = File.ReadAllText(MARKERS_YAML_FILE_PATH);
@@ -105,71 +164,36 @@ namespace DynmapMarkerPrune
             IOrderedDictionary<YamlNode, YamlNode> markersRoot = ((YamlMappingNode)sets["markers"]).Children;
             markers = ((YamlMappingNode)markersRoot["markers"]).Children;
             circles = ((YamlMappingNode)markersRoot["circles"]).Children;
+            
+            Console.WriteLine("markers.yml loaded...");
         }
 
 
-        private static void ProcessMarkersInRegion(int regionX, int regionZ)
+        private static bool IsMarkerInRegion(int regionX, int regionZ, int markerX, int markerZ)
         {
             int startX = regionX * 512;
             int startZ = regionZ * 512;
             int endX = startX + 512;
             int endZ = startZ + 512;
 
-            ProcessMarkersInRect(startX, startZ, endX, endZ);
-            processedChunks += 1024;
+            return CheckMarkerRectOverlap(startX, startZ, endX, endZ, markerX, markerZ);
         }
 
 
-        private static void ProcessMarkersInChunk(int chunkX, int chunkZ)   //TODO: Process all markers rather than areas. Loop markers.
+        private static bool IsMarkerInChunk(int chunkX, int chunkZ, int markerX, int markerZ)
         {
             int startX = chunkX * 16;
             int startZ = chunkZ * 16;
             int endX = startX + 16;
             int endZ = startZ + 16;
 
-            ProcessMarkersInRect(startX, startZ, endX, endZ);
-            processedChunks++;
+            return CheckMarkerRectOverlap(startX, startZ, endX, endZ, markerX, markerZ);
         }
 
 
-        private static void ProcessMarkersInRect(int rectMinX, int rectMinZ, int rectMaxX, int rectMaxZ)
+        private static bool CheckMarkerRectOverlap(int rectMinX, int rectMinZ, int rectMaxX, int rectMaxZ, int markerX, int markerZ)
         {
-            foreach (KeyValuePair<YamlNode, YamlNode> circleNode in circles)
-                CheckMarkerRectOverlap(rectMinX, rectMinZ, rectMaxX, rectMaxZ, circleNode);
-
-            foreach (KeyValuePair<YamlNode, YamlNode> markerNode in markers)
-                CheckMarkerRectOverlap(rectMinX, rectMinZ, rectMaxX, rectMaxZ, markerNode);
-        }
-
-
-        private static void CheckMarkerRectOverlap(int rectMinX, int rectMinZ, int rectMaxX, int rectMaxZ, KeyValuePair<YamlNode, YamlNode> markerNode)
-        {
-            YamlNode key = markerNode.Key;
-            YamlMappingNode properties = (YamlMappingNode)markerNode.Value;
-
-            if (!properties["world"].Equals(WorldNodeReference))
-                return;
-
-            (int x, int z) = GetCoordinates(properties);
-
-            switch (MODE)
-            {
-                case MarkerPruneMode.REMOVE_SELECTION_INCLUSIVE:
-                    if (x <= rectMinX || x >= rectMaxX || z <= rectMinZ || z >= rectMaxZ)
-                        return;
-                    break;
-                case MarkerPruneMode.REMOVE_SELECTION_EXCLUSIVE:
-                    if (x > rectMinX && x < rectMaxX && z > rectMinZ && z < rectMaxZ)
-                        return;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            if (!MarkersToDelete.Add(key.ToString()))
-                return;
-
-            Console.WriteLine($"Found marker to delete: {key}   (Label: {properties["label"]} | X: {x}, Z: {z})");
+            return markerX >= rectMinX && markerX < rectMaxX && markerZ >= rectMinZ && markerZ < rectMaxZ;
         }
 
 
@@ -220,12 +244,12 @@ namespace DynmapMarkerPrune
                 if (((YamlMappingNode)mMarkersRoot["markers"]).Children.ContainsKey(markerKeyToDelete))
                 {
                     ((YamlMappingNode)mMarkersRoot["markers"]).Children.Remove(markerKeyToDelete);
-                    Console.WriteLine($"Removed marker: {markerKeyToDelete} ({label})");
+                    //Console.WriteLine($"Removed marker: {markerKeyToDelete} ({label})");
                 }
                 else if (((YamlMappingNode)mMarkersRoot["circles"]).Children.ContainsKey(markerKeyToDelete))
                 {
                     ((YamlMappingNode)mMarkersRoot["circles"]).Children.Remove(markerKeyToDelete);
-                    Console.WriteLine($"Removed marker: {markerKeyToDelete} ({label})");
+                    //Console.WriteLine($"Removed marker: {markerKeyToDelete} ({label})");
                 }
             }
             
